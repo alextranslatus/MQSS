6732eb32b46cf9abb012fc109761a292ace49d6d
Alex Sheridan <alexsheridan.pro@outlook.fr>
2026-01-12 11:39:38 +0100

    Initial commit

---
+++ script.R
@@ -0,0 +1,14 @@
+load("IE0243AN_donnees_anonymes.RData")
+
+labels_table <- data.frame(
+  variable = names(data),
+  label    = sapply(data, function(x) attr(x, "label")),
+  stringsAsFactors = FALSE
+)
+
+labels_table <- subset(labels_table, !is.na(label))
+
+labels_table
+
+
+table(data$GROUP1, useNA = "always")
