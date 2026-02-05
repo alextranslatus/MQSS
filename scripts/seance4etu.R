4eaaa14ca4c7f7b67d468abac5ae7d565567500c
Alex Sheridan <alexsheridan.pro@outlook.fr>
2026-02-05 12:56:40 +0100

    script4

--- scripts/seance4etu.R
+++ scripts/seance4etu.R
@@ -1,15 +1,24 @@
 # QUELQUES OPERATIONS DE BASE SUR R ####
 
 # du calcul
-
+2+2
 
 # création d'objets quanti et quali
+x <- 2+2
+y <- 6
+x + y
 
+z <- "Chien"
+races <- c("Jack", "Labrador", "Teckel")
+races
 
 # création de vecteurs = le principe des variables
 
 # il est alors possible d'effectuer des opérations mathématiques sur ces vecteurs et d'en faire une analyse statistique
 
+tailles <- c(156, 202, 150)
+tailles / 100
+tailles
 
 
 # CHARGEMENT DE LA BASE ####
@@ -25,48 +34,77 @@ etupol <- as.data.table(etupol_1402_net)
 ## DESCRIPTION DE LA BASE ####
 
 # nombre de lignes
-
+nrow(etupol)
 # nombre de colonnes
-
+ncol(etupol)
 # synthèse des deux infos précédentes
-
+dim(etupol)
 # nom de l'ensemble des variables de la base
+names(etupol)
 
 ## ANALYSE UNIVARIEE ####
 
 # tableau des modalités d'une variable quali ou quanti, ou tri à plat
-
-
+table(etupol$STATUT_MERE, useNA = "always")
+table(etupol$AGE)
 # qu'est-ce qui cloche ici ? valeurs aberrantes# Toutefois, ils ne sont pas nombreux donc cela ne changera pas grand chose à l'analyse.
 # On peut donc les laisser en l'état, ou les recoder, par exemple en non réponse (NA)
-# Ici, les modalités "aberrantes" = NA
-# On vérifie
+
+str(etupol$AGE)
+
+
+etupol$AGE <- as.numeric(etupol$AGE)
+
+str(etupol$AGE)
+
+etupol[AGE > 100 | AGE < 17, AGE:= NA]
+
+  # Ici, les modalités "aberrantes" = NA
+table(etupol$AGE, useNA = "always") # On vérifie
 
 # si l'on veut obtenir les fréquences de répartition d'une variable
+install.packages("questionr")
+library(questionr)
 
+freq(etupol$AGE)
+freq(etupol$STATUT_MERE)
+
+round(prop.table(table(etupol$STATUT_MERE))*100, 1)
 
 # La colonne n représente les effectifs de chaque catégorie, la colonne % le pourcentage, et la colonne val% le pourcentage calculé sur les valeurs valides, donc en excluant les NA.
 # Une ligne a également été rajoutée pour indiquer le nombre et la proportion de NA.
 
 # moyenne d'une variable
-
-
+mean(etupol$STATUT_MERE)
 # marche pas : pourquoi ?
 
 # Type de variables : charactère, numérique ou facteur
-
+str(etupol$AGE)
+str(etupol$STATUT_MERE)
 
 # Permet d'afficher les premières lignes de la base 
-
+head(etupol$STATUT_MERE)
 
 # Permet d'afficher les dernières lignes de la base 
+tail(etupol$STATUT_MERE)
 
 # REPRESENTATIONS GRAPHIQUES ####
 
+hist(etupol$AGE)
+hist(etupol$STATUT_MERE)
 # marche pas, pourquoi ?
 
 # on va utiliser un truc plus flexible : ggplot2
+install.packages("ggplot2")
+library(ggplot2)
 
+ggplot(etupol, aes(x = STATUT_MERE)) +
+  geom_bar(fill = "lightblue") +
+  theme_minimal() +
+  labs(title = "Statut de la mère",
+       x = "",
+       y = "Fréqence") +
+  coord_flip()
 
 # Quelques éléments pour compléter la séance 3 ####
 
@@ -109,7 +147,13 @@ etupol <- as.data.table(etupol_1402_net)
 
 
 # 2. Réalisez un histogramme pour la variable ACTUALITES_CLIMAT
-
+ggplot(etupol, aes(x = ACTUALITES_CLIMAT)) +
+  geom_bar(fill = "lightblue") +
+  labs(x = "", 
+       y = "Fréquence", 
+       title = "Réaction face aux enjeux climatiques") +
+  theme_minimal() +
+  coord_flip()
 
 # 3. Créez deux bases de données, l'une n'incluant que les femmes, l'autre que les hommes, en utilisant la fonction subset
 # la fonction prend la forme subset(base, variable == "Nom")
@@ -124,6 +168,34 @@ etupol <- as.data.table(etupol_1402_net)
 # 5. Recoder la variable ACTUALITES_CLIMAT en une variable avec deux niveaux, en 
 # regroupant Ne sait pas / non concerné et Je n'ai pas réagi dans un niveau, et
 # J'ai partagé, commenté ou réagi dans un autre niveau
+table(etupol$ACTUALITES_CLIMAT)
+etupol$reaction <- NA
+etupol[ACTUALITES_CLIMAT == "J’ai partagé, commenté ou réagi (liké)", reaction:= "J’ai partagé, commenté ou réagi (liké)"]
+etupol[ACTUALITES_CLIMAT %in% c("Ne sait pas / non concerné", "Je n’ai pas réagi"), reaction:= "Je n’ai pas réagi"]
+table(etupol$reaction)
+
+basef <- subset(etupol, etupol$GENRE == "Une femme")
+baseh <- subset(etupol, etupol$GENRE == "Un homme")
+
+dim(basef)
+dim(baseh)
+
+ggplot(basef, aes(x = ACTUALITES_CLIMAT)) +
+  geom_bar(fill = "lightblue") +
+  labs(x = "", 
+       y = "Fréquence", 
+       title = "Réaction des femmes face aux enjeux climatiques") +
+  theme_minimal() +
+  coord_flip()
+
+ggplot(baseh, aes(x = ACTUALITES_CLIMAT)) +
+  geom_bar(fill = "lightblue") +
+  labs(x = "", 
+       y = "Fréquence", 
+       title = "Réaction des hommes face aux enjeux climatiques") +
+  theme_minimal() +
+  coord_flip()
+
 
 # Vous pouvez reprendre les étapes 2 à 4 avec cette variable binaire
 
